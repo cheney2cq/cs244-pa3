238e99ea440b8f0d281564b8b3c11b3d41d5bbab
CJ Cullen <cjcullen888@gmail.com>
2014-05-27 04:53:47 +0000

    Adjusted topology to closer match results from paper.

--- topo.py
+++ topo.py
@@ -14,9 +14,9 @@ class Wifi3GTopo(Topo):
         s1 = self.addSwitch('s1')
 
         # WiFi link
-        self.addLink(h1, s0, bw=10, delay='10ms', loss=1.5, jitter='2ms', max_queue_size=1000)
+        self.addLink(h1, s0, bw=20, delay='5ms', loss=0.5, jitter='0ms', max_queue_size=1000)
         # 3G link
-        self.addLink(h1, s1, bw=4, delay='65ms', loss=1.0, jitter='35ms', max_queue_size=1000)
+        self.addLink(h1, s1, bw=2, delay='65ms', loss=0.5, jitter='35ms', max_queue_size=1000)
 
         self.addLink(s0, h2, bw=1000, delay='1ms', loss=0, max_queue_size=10)
         self.addLink(s1, h2, bw=1000, delay='1ms', loss=0, max_queue_size=10)
